6626ae992f03db57e7a419826968c37981a3dba6
Nguyễn Trọng Thức <24110349@student.hcmute.vn>
2026-08-24 02:19:21 +0700

    Create Connect DBMS

---
+++ DBMS/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace DBMS
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnOpeConnect = new Button();
+            btnCloseConnect = new Button();
+            SuspendLayout();
+            // 
+            // btnOpeConnect
+            // 
+            btnOpeConnect.Location = new Point(21, 31);
+            btnOpeConnect.Name = "btnOpeConnect";
+            btnOpeConnect.Size = new Size(124, 37);
+            btnOpeConnect.TabIndex = 0;
+            btnOpeConnect.Text = "Mở Kết Nối";
+            btnOpeConnect.UseVisualStyleBackColor = true;
+            btnOpeConnect.Click += btnOpeConnect_Click;
+            // 
+            // btnCloseConnect
+            // 
+            btnCloseConnect.Location = new Point(21, 109);
+            btnCloseConnect.Name = "btnCloseConnect";
+            btnCloseConnect.Size = new Size(124, 37);
+            btnCloseConnect.TabIndex = 1;
+            btnCloseConnect.Text = "Đóng Kết Nối";
+            btnCloseConnect.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnCloseConnect);
+            Controls.Add(btnOpeConnect);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnOpeConnect;
+        private Button btnCloseConnect;
+    }
+}
---
+++ DBMS/Form1.cs
@@ -0,0 +1,41 @@
+using System.Data.SqlClient; // Thư viện cũ quá để làm việc 
+using Microsoft.Data.SqlClient; // Thư viện mới để làm việc với SQL Server
+// Khai báo thư viện để kết nối với cơ sở dữ liệu SQL Server
+
+using System.Data; // Thư viện để làm việc với dữ liệu và các đối tượng liên quan đến cơ sở dữ liệu
+
+namespace DBMS
+{
+    public partial class Form1 : Form
+    {
+        String strCon = @"Data Source=DESKTOP-LPF0IL9;Initial Catalog=Test;Integrated Security=True;Trust Server Certificate=True";
+        // Chuỗi kết nối đến cơ sở dữ liệu SQL Server - như địa chỉ máy chủ, tên cơ sở dữ liệu, thông tin xác thực, v.v.
+        SqlConnection sqlCon = null;
+        // Đối tượng SqlConnection để quản lý kết nối đến cơ sở dữ liệu
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpeConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                sqlCon = new SqlConnection(strCon);
+                // Tạo đối tượng SqlConnection để kết nối đế Cở Sở Dữ Liệu SQL Server
+
+                if(sqlCon.State == ConnectionState.Closed)
+                {
+                    sqlCon.Open();
+                    MessageBox.Show("Kết nối thành công");
+                }
+            }
+            catch (Exception ex)
+            {
+                // Hiển thị thông báo lỗi nếu có ngoại lệ xảy ra
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
